c9db7d9d4858b0b7ba1843e8473fa8d3a89deca9
bekirerenpeker <bekirerenpeker@gmail.com>
2026-06-15 20:37:50 +0300

    cleaned test scene

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -6,20 +6,5 @@ int main()
 {
     Logger::get().addSink<FileSink>("log.txt");
 
-    float freq = 10.f;
-    ImageData imgData(1000, 1000, 1);
-    for (int x = 0; x < imgData.width; x++) {
-        for (int y = 0; y < imgData.height; y++) {
-            imgData.setValueAt(
-                x, y, 0,
-                Math::perlin2D(x / (float)imgData.width * freq, y / (float)imgData.height * freq) *
-                    256
-            );
-        }
-    }
-    FileManager::get().createFile("test_image.png");
-    ImageFile file("test_image.png");
-    file.saveImage(imgData);
-
     return 0;
 }
