f1af7506110b2ccb8dd1dc77c3fef64c2d054924
bekirerenpeker <bekirerenpeker@gmail.com>
2026-06-18 22:09:24 +0300

    added a piano demo to show off the audio engine

---
+++ game/include/piano_demo.hpp
@@ -0,0 +1,46 @@
+#include "EngineInclude.hpp"
+using namespace Engine;
+
+int piano_demo()
+{
+    IdType windowId = WindowManager::get().createWindow({1000, 800, "test window"});
+
+    KeyCode keyCodes[13] = {KeyCode::A, KeyCode::W, KeyCode::S, KeyCode::D, KeyCode::R,
+                            KeyCode::F, KeyCode::T, KeyCode::G, KeyCode::H, KeyCode::U,
+                            KeyCode::J, KeyCode::I, KeyCode::K};
+    fs::path dir = "game/assets/audio/piano_notes";
+    fs::path files[13] = {"a1.wav", "a1s.wav", "b1.wav",  "c1.wav", "c1s.wav", "d1.wav", "d1s.wav",
+                          "e1.wav", "f1.wav",  "f1s.wav", "g1.wav", "g1s.wav", "c2.wav"};
+    AudioBuffer* resources[13] = {nullptr};
+    IdType instances[13] = {INVALID_ID};
+
+    AudioManager::get();   // initalize the audio engine
+    for (int i = 0; i < 13; i++) {
+        IdType id = ResourceManager::get().addResource<AudioBuffer>(dir / files[i]);
+        resources[i] = ResourceManager::get().getResource<AudioBuffer>(id);
+    }
+
+    Window* mainWindow = WindowManager::get().getMainWindow();
+    while (mainWindow->isOpen()) {
+        Time::get().update();
+        Input::get().update(windowId);
+
+        if (Input::get().keyPressed(KeyCode::Q)) break;
+
+        for (int i = 0; i < 13; i++) {
+            if (Input::get().keyPressed(keyCodes[i])) {
+                instances[i] = AudioManager::get().playAudio(resources[i]);
+            }
+            if (Input::get().keyReleased(keyCodes[i]) &&
+                AudioManager::get().getAudioInstance(instances[i])) {
+                // AudioManager::get().stopAudioInstance(instances[i]);
+            }
+        }
+
+        mainWindow->swapBuffers();
+        GlfwContext::pollEvents();
+    }
+
+    AudioManager::get().shutdown();   // terminate it before the resource manager terminates
+    return 0;
+}
--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,57 +1,10 @@
 #include "EngineInclude.hpp"
+#include "piano_demo.hpp"
 
 using namespace Engine;
 
 int main()
 {
     Logger::get().addSink<FileSink>("game/output/log.txt");
-
-    IdType windowId = WindowManager::get().createWindow({1000, 800, "test window"});
-
-    AudioManager::get();   // initalize the audio engine
-    IdType soundId =
-        ResourceManager::get().addResource<AudioBuffer>("game/assets/audio/CoinPickup.wav");
-    IdType musicId = ResourceManager::get().addResource<AudioStream>("game/assets/audio/Music.mp3");
-    auto* sound = ResourceManager::get().getResource<AudioBuffer>(soundId);
-    auto* music = ResourceManager::get().getResource<AudioStream>(musicId);
-    IdType musicInstanceId = AudioManager::get().playAudio(music);
-
-    Window* mainWindow = WindowManager::get().getMainWindow();
-    while (mainWindow->isOpen()) {
-        Time::get().update();
-        Input::get().update(windowId);
-
-        if (Input::get().keyPressed(KeyCode::Q)) break;
-        if (Input::get().keyPressed(KeyCode::S)) AudioManager::get().playAudio(sound);
-        if (Input::get().keyPressed(KeyCode::C)) {
-            AudioManager::get().stopAudioInstance(musicInstanceId);
-        }
-
-        if (AudioManager::get().getAudioInstance(musicInstanceId)) {
-            LOG_INFO(
-                "{}/{}", AudioManager::get().getCursorSeconds(musicInstanceId),
-                AudioManager::get().getDurationSeconds(musicInstanceId)
-            );
-
-            if (Input::get().keyPressed(KeyCode::Space)) {
-                bool paused = AudioManager::get().isPaused(musicInstanceId);
-                AudioManager::get().setIsPaused(musicInstanceId, !paused);
-            }
-
-            if (Input::get().keyPressed(KeyCode::Right)) {
-                float current = AudioManager::get().getCursorSeconds(musicInstanceId);
-                AudioManager::get().setCursorSeconds(musicInstanceId, current + 10.0f);
-            }
-            if (Input::get().keyPressed(KeyCode::Left)) {
-                float current = AudioManager::get().getCursorSeconds(musicInstanceId);
-                AudioManager::get().setCursorSeconds(musicInstanceId, current - 10.0f);
-            }
-        }
-
-        mainWindow->swapBuffers();
-        GlfwContext::pollEvents();
-    }
-
-    AudioManager::get().shutdown();   // terminate it before the resource manager terminates
-    return 0;
+    return piano_demo();
 }
